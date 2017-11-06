0965d172d1fcacab1bbd7d4988b7240d468feb87
archana <archana.nayak@gmail.com>
2017-11-05 23:41:13 -0800

    "Add generic function call for all card number generation"

--- detectNetwork.js
+++ detectNetwork.js
@@ -43,138 +43,88 @@ var detectNetwork = function(cardNumber) {
   	}
 	return undefined;
 };
+
+
 // China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19.
 var chinaUnionNumbers = [];
 var chinaArrayRange1 = ["622126","622925"];//prefix.length = 6
-var chinaLenArray1 = [10,11,12,13];//[16-19](Range) - 6 
-unionPayNumberGenerator(chinaArrayRange1,chinaLenArray1,chinaUnionNumbers);
-
+var chinaLenArray = [16,17,18,19]; 
+unionPayNumberGenerator(chinaArrayRange1,chinaLenArray,chinaUnionNumbers);
 var chinaArrayRange2 = ["624","626"];
-var chinaLenArray2 = [13,14,15,16];
-unionPayNumberGenerator(chinaArrayRange2,chinaLenArray2,chinaUnionNumbers);
-
+unionPayNumberGenerator(chinaArrayRange2,chinaLenArray,chinaUnionNumbers);
 var chinaArrayRange3 = ["6282","6288"];
-var chinaLenArray3 = [12,13,14,15];
-unionPayNumberGenerator(chinaArrayRange3,chinaLenArray3,chinaUnionNumbers);
-
+unionPayNumberGenerator(chinaArrayRange3,chinaLenArray,chinaUnionNumbers);
 
 function unionPayNumberGenerator(chinaArrayRange, chinaLenArray,chinaUnionNumbers){
 for(var prefix = Number(chinaArrayRange[0]); prefix <= Number(chinaArrayRange[1]); prefix++){
   for(var j = 0; j < chinaLenArray.length;j++){
     var number = prefix.toString();
-    for(var k = 0; k < chinaLenArray[j];k++){
+    for(var k = 0; k < chinaLenArray[j] - prefix.toString().length;k++){
       number = number.concat(Math.floor(Math.random() * 10));
     }
     chinaUnionNumbers.push(number);
-    detectNetwork(number);
+    var resultString = detectNetwork(number);
+    console.log(resultString + " " + number);
   }
 }
 }
 
-//Number generation for Diner's
+// //Number generation for Diner's
 var dinerNumbers = [];
-var dinerLength = 12;
+var dinerLength = [14];
 var dinerPrefix = ['38','39'];
-for(var i = 0; i < dinerPrefix.length;i++){
-  var number = dinerPrefix[i];
-  for(var j = 0; j < dinerLength; j++){
-    number = number.concat(Math.floor(Math.random() * 10));
-  }
-  detectNetwork(number);
-  dinerNumbers.push(number);
-}
+callGenericGeneratorForMultipleLengths(dinerPrefix,dinerLength,dinerNumbers);
 
-// //Number generation for Mastercard
+//Number generation for Mastercard
 var mcNumbers = [];
-var mcLength = 14;
+var mcLength = [16];
 var mcPrefix = ['51','52','53','54','55'];
-for(var i = 0; i < mcPrefix.length;i++){
-  var number = mcPrefix[i];
-  for(var j = 0; j < mcLength; j++){
-    number = number.concat(Math.floor(Math.random() * 10));
+callGenericGeneratorForMultipleLengths(mcPrefix,mcLength,mcNumbers);
+ 
+  //Number generation for visa
+var visaNumbers = [];
+var visaLength = [13,16,19];
+var visaPrefix = ['4'];
+callGenericGeneratorForMultipleLengths(visaPrefix,visaLength,visaNumbers);
+
+function callGenericGeneratorForMultipleLengths(prefixArray,lengthArray,numbers){
+  for(var i = 0; i < prefixArray.length;i++){
+    for(var k = 0; k < lengthArray.length; k++){
+     var number = prefixArray[i];
+     for(var j = 0; j < lengthArray[k] - prefixArray[i].length; j++){
+      number = number.concat(Math.floor(Math.random() * 10));
+     }
+     var resultString = detectNetwork(number);
+     console.log(resultString +" " + number);
+     numbers.push(number);
+    }
   }
-  detectNetwork(number);
-  mcNumbers.push(number);
 }
+//Number generation for american express
+var americanNumbers = [];
+var americanLength = [15];
+var americanPrefix = ['34','37'];
+callGenericGeneratorForMultipleLengths(americanPrefix,americanLength,americanNumbers);
 
 // Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
 //Number generation for Switch
 var sNumbers = [];
-var sLength = [12,14,15];
+var sLength = [16,18,19];
 var sPrefix = ['4903','4905','4911','4936','6333','6759'];
-switchCardGenerator(sPrefix,sLength,sNumbers);
-sLength = [10,12,13];
+callGenericGeneratorForMultipleLengths(sPrefix,sLength,sNumbers);
 sPrefix = ['564182', '633110'];
-switchCardGenerator(sPrefix,sLength,sNumbers);
+callGenericGeneratorForMultipleLengths(sPrefix,sLength,sNumbers);
 
-function switchCardGenerator(prefixArray,lengthArray,numbers) {
-  for(var i = 0; i < prefixArray.length;i++){
-    for(var k = 0; k < lengthArray.length; k++){
-      var number = prefixArray[i];
-      for(var j = 0; j < lengthArray[k]; j++){
-        number = number.concat(Math.floor(Math.random() * 10));
-      }
-      detectNetwork(number);
-      numbers.push(number);
-    }
-  }
-}
 
-//Number generation for visa
-var visaNumbers = [];
-var visaLength = [12,15,18];
-var visaPrefix = ['4'];
-for(var i = 0; i < visaPrefix.length;i++){
-  for(var k = 0; k < visaLength.length; k++){
-  var number = visaPrefix[i];
-  for(var j = 0; j < visaLength[k]; j++){
-    number = number.concat(Math.floor(Math.random() * 10));
-  }
-  detectNetwork(number);
-  visaNumbers.push(number);
-}
-}
-
-//Number generation for american express
-var americanNumbers = [];
-var americanLength = 13;
-var americanPrefix = ['34','37'];
-
-for(var i = 0; i < americanPrefix.length;i++){
-  var number = americanPrefix[i];
-  for(var j = 0; j < americanLength; j++){
-    number = number.concat(Math.floor(Math.random() * 10));
-  }
-  detectNetwork(number);
-  americanNumbers.push(number);
-}
-
-// //var discover =  {name: "Discover", prefix :[6011,644,645,646,647,648,649,65], length :[16,19]};
+// // //var discover =  {name: "Discover", prefix :[6011,644,645,646,647,648,649,65], length :[16,19]};
 // //number generation for Discover
 var discoverNumbers = [];
-var discoverLenArray = [12,15];
-var discoverPrefixArray = ["6011","6440","6450","6460","6470","6480","6490","6589"];
-for(var k = 0; k < discoverPrefixArray.length; k++){
-  for(var i = 0; i < discoverLenArray.length; i++){
-    var discoverNumber = discoverPrefixArray[k];
-    for(var j = 0; j < discoverLenArray[i]; j++){
-      discoverNumber = discoverNumber.concat(Math.floor(Math.random() * 10));
-    }
-    detectNetwork(discoverNumber);
-    discoverNumbers.push(discoverNumber);
-  }
-}
+var discoverLenArray = [16,19];
+var discoverPrefixArray = ["6011","644","645","646","647","648","649","65"];//padded each string with numbers to make length of all strings 4 to match the length of 6011
+callGenericGeneratorForMultipleLengths(discoverPrefixArray,discoverLenArray,discoverNumbers);
+
 // //Number generation for Maestro
 var maestroNumbers = [];
-var lenArray = [8,9,10,11,12,13,14,15];
+var lenArray = [12,13,14,15,16,17,18,19];
 var prefixArray = ["5018","5020","5038","6304"];
-for (var k = 0; k < prefixArray.length; k++) {
-  for(var i = 0; i < lenArray.length;i++){
-    var beginning = prefixArray[k];
-    for(var j = 0; j < lenArray[i]; j++){
-      beginning = beginning.concat(Math.floor(Math.random()*10));
-    }
-    detectNetwork(beginning);
-    maestroNumbers.push(beginning);
-  }
-}
+callGenericGeneratorForMultipleLengths(prefixArray,lenArray,maestroNumbers);
